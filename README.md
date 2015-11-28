25c946cb3bb4b913aef35be721eaaec7671b6f3f
greenbreakfast <lazardemin@gmail.com>
2015-11-27 19:15:53 -0500

    Added rpcd script to integrate i2c-exp drivers into ubus, updated readme to reflect the programming

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # i2c-exp-driver
 Project for drivers to program Omega Expansions that are I2C-based.
+
+## ubus Integration
+The drivers for the I2C-based Onion Expansions have been integrated with the ubus service via RPCD. They can be accessed through the i2c_exp service.
+
+### pwm-exp ubus Commands
+The Servo Expansion can be programmed using the `pwm-exp` command in the i2c_exp ubus service:
+```bash
+ubus call i2c_exp pwm-exp '{"command":"String", "params": {"key": "value" } }'
+```
+
+The following commands are available:
+* init
+* sleep
+* set
+* set-period
+
+#### Init Command
+The `init` command will reset the chip on the Servo Expansion and enable the oscillator.
+```bash
+ubus call i2c_exp pwm-exp '{"command":"init"}'
+```
+
+#### Sleep Command
+The `sleep` command will put the oscillator into sleep mode, disabling all PWM signals
+```bash
+ubus call i2c_exp pwm-exp '{"command":"sleep"}'
+```
+
+#### Set Command
+The `set` command is used to generate a PWM signal on a specific channel based on a **duty cycle percentage**:
+```bash
+ubus call i2c_exp pwm-exp '{"command":"set", "params":{"channel":"<CHANNEL>", "duty":"<DUTY CYCLE>"} }'
+```
+
+The CHANNEL can be:
+* 0 - 15
+  * control an individual channel
+* all
+  * control all channels at once
+
+The DUTY CYCLE can be an integer or floating point number between 0 and 100
+
+Optional parameters:
+* `"frequency":"<signal frequency>"`
+  * Sets the PWM signal frequency
+  * If not specified, default of 50 Hz is used
+  * Frequency range is 24 Hz to 1526 Hz
+* `"delay":"<delay percentage>"`
+  * Adds a delay in the PWM signal, can be an integer or floating point number between 0 and 1000
+  * 0% delay by default
+
+#### Set-Period Command
+The `set-period` command is used to generate a PWM signal on a specific channel based on a **PWM period and pulse width**:
+```bash
+ubus call i2c_exp pwm-exp '{"command":"set-period", "params":{"channel":9, "pulse":"1.5", "periods":20}}'
+```
+
+
+### relay-exp ubus Commands
+The Relay Expansion can be programmed using the `relay-exp` command in the i2c_exp ubus service:
+
+#### Set Command
+The only command is the `set` command, it allows the relays to be programmed:
+```bash
+ubus call i2c_exp relay-exp '{"command":"set", "params":{"channel":"<CHANNEL>", "state":"RELAY STATE"}}'
+```
+
+The CHANNEL can be one of:
+* 0		- controls Relay0
+* 1		- controls Relay1
+* all 	- controls both relays
+
+The RELAY STATE can be one of:
+* off	- relay is switched off
+* on	- relay is switched on
+
---
+++ rpcd/i2c-exp.sh
@@ -0,0 +1,184 @@
+#!/bin/sh
+
+# include the Onion sh lib
+. /usr/lib/onion/lib.sh
+#dbg
+bLogEnabled=1
+
+
+##### pwm-exp #####
+# function to set a pwm channel with duty cycle
+PwmExpSet () {
+	# select the arguments object
+	json_select params
+	# parse the arguments
+	json_get_var channel "channel"
+	json_get_var duty "duty"
+	json_get_var delay "delay"
+	json_get_var freq "frequency"
+	if [ "$freq" != "" ]; then
+		freq="-f $freq"
+	fi
+
+	# check arguments
+	if 	[ "$channel" == "" ]; then
+		ret="missing channel argument"
+	elif [ "$duty" == "" ]; then
+		ret="missing duty argument"
+	else
+		ret=$(/usr/sbin/pwm-exp -q $freq $channel $duty $delay)
+	fi
+	Log "pwm-exp: set $freq $channel $duty $delay"
+
+	echo "$ret"
+}
+
+# function to set a pwm channel with pulse width and period
+PwmExpSetPeriod () {
+	# select the arguments object
+	json_select params
+	# parse the arguments object
+	json_get_var channel "channel"
+	json_get_var pulse "pulse"
+	json_get_var period "period"
+
+	# check arguments
+	if 	[ "$channel" == "" ]; then
+		ret="missing channel argument"
+	elif [ "$pulse" == "" ]; then
+		ret="missing pulse argument"
+	elif [ "$period" == "" ]; then
+		ret="missing period argument"
+	else
+		ret=$(/usr/sbin/pwm-exp -q -p $channel $pulse $period)
+	fi
+	Log "pwm-exp: set -p $channel $pulse $period"
+
+	echo "$ret"
+}
+
+# main pwm-exp function
+PwmExpMain () {
+	# read the command
+	json_get_var cmd "command"
+
+	# specific commands
+	case "$cmd" in
+		init)
+			ret=$(/usr/sbin/pwm-exp -q -i)
+			Log "pwm-exp: init, ret is $ret"
+		;;
+		sleep)
+			ret=$(/usr/sbin/pwm-exp -q -s)
+			Log "pwm-exp: sleep, ret is $ret"
+		;;
+		set)
+			ret=$(PwmExpSet)
+		;;
+		set-period)
+			ret=$(PwmExpSetPeriod)
+		;;
+	esac
+
+	# print return message
+	if [ "$ret" == "" ]; then
+		echo '{"status":"success"}'
+	else
+		echo '{"status":"failure", "error":"'"$ret"'"}'
+	fi
+}
+
+##### relay-exp #####
+# main relay-exp function
+RelayExpMain () {
+	# read the command
+	json_get_var cmd "command"
+
+	# only command is 'set'	
+	if [ "$cmd" == "set" ]
+	then
+		# select the arguments object
+		json_select params
+		# parse the arguments object
+		json_get_var channel "channel"
+		json_get_var state "state"
+		json_get_var addr "address"
+		if [ "$addr" != "" ]; then
+			addr="-s $addr"
+		fi
+
+		# check arguments
+		if 	[ "$channel" == "" ]; then
+			ret="missing channel argument"
+		elif [ "$state" == "" ]; then
+			ret="missing state argument"
+		else
+			ret=$(/usr/sbin/relay-exp -q $addr $channel $state)
+		fi
+		Log "relay-exp: set $addr $channel $state"
+	else
+		ret="invalid command"
+	fi
+	
+	# print return message
+	if [ "$ret" == "" ]; then
+		echo '{"status":"success"}'
+	else
+		echo '{"status":"failure", "error":"'"$ret"'"}'
+	fi
+}
+
+
+
+########################
+##### Main Program #####
+
+cmdPwmExp="pwm-exp"
+cmdRelayExp="relay-exp"
+cmdStatus="status"
+
+jsonPwmExp='"'"$cmdPwmExp"'": { "command":"cmd", "params": {"key": "value" } }'
+jsonRelayExp='"'"$cmdRelayExp"'": { "command":"cmd", "params": {"key": "value" } }'
+jsonStatus='"'"$cmdStatus"'": { }'
+
+
+case "$1" in
+    list)
+		echo "{ $jsonPwmExp, $jsonRelayExp, $jsonStatus }"
+    ;;
+    call)
+		Log "Function: call, Method: $2"
+
+		case "$2" in
+			$cmdPwmExp)
+				# read the json arguments
+				read input;
+				Log "Json argument: $input"
+
+				# parse the json
+				json_load "$input"
+
+				# parse the json and run the function
+				PwmExpMain
+			;;
+			$cmdRelayExp)
+				# read the json arguments
+				read input;
+				Log "Json argument: $input"
+
+				# parse the json
+				json_load "$input"
+
+				# parse the json and run the function
+				RelayExpMain
+			;;
+			$cmdStatus)
+				# dummy call for now
+				echo '{"status":"good"}'
+		;;
+		esac
+    ;;
+esac
+
+# take care of the log file
+CloseLog
